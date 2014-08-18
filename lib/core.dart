177e7f84c5c2dbb021a53a08b0b9d0e570e30bc8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-17 20:20:13 -0400

    This probably isn't good for anything but it was fun to make.

--- lib/core.dart
+++ lib/core.dart
@@ -1,5 +1,8 @@
 library alpha.core;
 
+import 'dart:math' show Random;
+
 part 'src/core/range.dart';
 part 'src/core/lists.dart';
 part 'src/core/logic.dart';
+part 'src/core/combination.dart';
---
+++ lib/src/core/combination.dart
@@ -0,0 +1,21 @@
+part of alpha.core;
+
+class CombinationLock {
+  final List<int> _code;
+  
+  CombinationLock(this._code);
+  
+  static CombinationLock createRandom(int size, void callback(List<int> code)) {
+    var code = [];
+    for (int i = 1; i<= size; i++) {
+      code.add(new Random().nextInt(9));
+    }
+    callback(code);
+    return new CombinationLock(code);
+  }
+  
+  bool unlock(List<int> code) {
+    return _code == code;
+  }
+}
+
