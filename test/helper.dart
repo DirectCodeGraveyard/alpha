85735c768d226845746bee68f16e4738b7442f7f
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-22 10:22:43 -0400

    Made Test Output Very Awesome

--- test/helper.dart
+++ test/helper.dart
@@ -1,13 +1,12 @@
 import "package:unittest/unittest.dart";
+import "package:unittest/src/utils.dart";
 
-import "dart:async";
 import "dart:io";
 
 typedef void TestGroup();
 
 class SuiteInfo {
   final String name;
-
   final Map<String, TestGroup> sections;
 
   SuiteInfo(this.name, {this.sections: const {}});
@@ -17,62 +16,99 @@ void runTests(List<SuiteInfo> modules) {
 
   unittestConfiguration = new AlphaTestConfiguration();
 
-  void printLine(Zone self, ZoneDelegate parent, Zone zone, String line) {
-    if (line.startsWith("unittest-")) return;
-    stdout.write(line);
+  for (var module in modules) {
+    group(module.name, () {
+      for (var name in module.sections.keys) {
+        group(name, module.sections[name]);
+      }
+    });
   }
-
-  runZoned(() {
-    for (var module in modules) {
-      group(module.name, () {
-        for (var name in module.sections.keys) {
-          group(name, module.sections[name]);
-        }
-      });
-    }
-  }, zoneSpecification: new ZoneSpecification(print: printLine));
 }
 
 void runSuite(SuiteInfo module) => runTests([module]);
-
 
 class AlphaTestConfiguration extends SimpleConfiguration {
   final String GREEN_COLOR = '\u001b[32m';
   final String RED_COLOR = '\u001b[31m';
   final String MAGENTA_COLOR = '\u001b[35m';
   final String NO_COLOR = '\u001b[0m';
-  
+
   @override
   void onInit() {
-    super.onInit();
     filterStacks = formatStacks = true;
   }
-  
+
   @override
   String formatResult(TestCase testCase) {
-    var result = super.formatResult(testCase);
-    if (testCase.result == PASS) {
-      return "${GREEN_COLOR}${result}${NO_COLOR}";
-    } else if (testCase.result == FAIL) {
-      return "${RED_COLOR}${result}${NO_COLOR}";
-    } else if (testCase.result == ERROR) {
-      return "${MAGENTA_COLOR}${result}${NO_COLOR}";
+    var buff = new StringBuffer();
+
+    var status = testCase.result;
+    
+    var color = "";
+    
+    switch (status) {
+      case PASS:
+        color = GREEN_COLOR;
+        break;
+      case FAIL:
+        color = RED_COLOR;
+        break;
+      case ERROR:
+        color = MAGENTA_COLOR;
+        break;
     }
-    return result;
+    
+    buff
+        ..write(color)
+        ..write(status.toUpperCase())
+        ..write(NO_COLOR)
+        ..write(": ")
+        ..write(testCase.description)
+        ..writeln();
+
+    if (testCase.message.isNotEmpty) {
+      buff.writeln(indent(testCase.message));
+    }
+
+    if (testCase.stackTrace != null) {
+      buff.writeln(indent(testCase.stackTrace.toString()));
+    }
+
+    return buff.toString();
   }
-  
+
+  @override
+  void onSummary(int passed, int failed, int errors, List<TestCase> results, String uncaughtError) {
+    for (final t in results) {
+      print(formatResult(t).trim());
+    }
+
+    print('');
+
+    if (passed == 0 && failed == 0 && errors == 0 && uncaughtError == null) {
+      print('No tests found.');
+    } else if (failed == 0 && errors == 0 && uncaughtError == null) {
+      print('All ${passed} tests passed.');
+    } else {
+      if (uncaughtError != null) {
+        print('Top-level uncaught error: ${uncaughtError}');
+      }
+      print('${passed} PASSED, ${failed} FAILED, ${errors} ERRORS');
+    }
+  }
+
   @override
   void onDone(bool success) {
     int status;
-    
+
     try {
-      super.onDone(success);
+      if (throwOnTestFailures) {
+        throw new Exception('Some tests failed.');
+      }
       status = 0;
     } catch (ex) {
       status = 1;
     }
-    
-    print("\n");
     
     exit(status);
   }
