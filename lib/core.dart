fe8a4854a8746f05deb60e4ad1965a5e71072273
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-06 14:32:45 -0400

    Fix Core Library

--- lib/core.dart
+++ lib/core.dart
@@ -1,9 +1,6 @@
 library alpha.core;
 
-import 'dart:math' show Random;
-
 part 'src/core/range.dart';
 part 'src/core/lists.dart';
 part 'src/core/logic.dart';
-part 'src/core/combination.dart';
 part 'src/core/functions.dart';
