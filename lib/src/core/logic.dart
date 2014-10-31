ea4b1e62778e5e91d28c7763c45729daab44b7b7
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-31 10:41:52 -0400

    Put logics in Logics class

--- lib/src/core/logic.dart
+++ lib/src/core/logic.dart
@@ -1,40 +1,42 @@
 part of alpha.core;
 
-bool and(bool a, bool b) =>
-    a && b;
+class Logics {
+  static bool and(bool a, bool b) =>
+      a && b;
 
-bool not(bool it) =>
-    it == false;
+  static bool not(bool it) =>
+      it == false;
 
-bool or(bool a, bool b) =>
-    a || b;
+  static bool or(bool a, bool b) =>
+      a || b;
 
-bool xor(bool a, bool b) =>
-    (a == true) && (b == false) || (a == false) && (b == true);
+  static bool xor(bool a, bool b) =>
+      (a == true) && (b == false) || (a == false) && (b == true);
 
-bool nand(bool a, bool b) =>
-    a != b;
+  static bool nand(bool a, bool b) =>
+      a != b;
 
-bool nor(bool a, bool b) =>
-    a == false && b == false;
+  static bool nor(bool a, bool b) =>
+      a == false && b == false;
 
-bool xnor(bool a, bool b) =>
-    a == b;
+  static bool xnor(bool a, bool b) =>
+      a == b;
 
-int increment(int i) =>
-    i + 1;
+  static int increment(int i) =>
+      i + 1;
 
-int decrement(int i) =>
-    i - 1;
+  static int decrement(int i) =>
+      i - 1;
 
-num add(num a, num b) =>
-    a + b;
+  static num add(num a, num b) =>
+      a + b;
 
-num subtract(num a, num b) =>
-    a - b;
+  static num subtract(num a, num b) =>
+      a - b;
 
-num multiply(num a, num b) =>
-    a * b;
+  static num multiply(num a, num b) =>
+      a * b;
 
-num divide(num a, num b) =>
-    a / b;
+  static num divide(num a, num b) =>
+      a / b;
+}
